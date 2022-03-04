a68d1c868640607701b81de54623400a386d1d53
fary <fbakhsheshi@ymail.com>
2022-03-04 11:28:41 +0100

    new change

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -47,4 +47,37 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-;
+; //   ---------------  Chat Bot ---------------------------
+
+var chatIcon = document.querySelector('#hero .hero .chat-icon');
+var chatBox = document.querySelector('#hero .hero .content');
+var closeBtn = document.querySelector('#hero .hero .content .header-chat .close-btn');
+var icon = document.querySelector('#hero .hero .chat-icon .fas'); // ------ Show content Chat Box ------
+// chatIcon.addEventListener('click', () => {
+//     chatBox.classList.toggle('active');
+//     if (icon.style.color != "#fff")
+//         icon.style.color = "#fff";
+//     icon.style.color = "#302929";
+// });
+// closeBtn.addEventListener('click', () => {
+//     chatBox.classList.toggle('active');
+//     icon.style.color = "#fff";
+// });
+// ------ Show Chat icon after 10 seconde -------
+// function showIcon() {
+//     chatIcon.style.display = "flex";
+// }
+// function changeColorIcon() {
+//     icon.style.color = "#302929";
+// };
+// setTimeout(() => {
+//     showIcon();
+// }, 10000);
+// ------ Show Chat Box after 10.5 seconde -------
+// function showChatBox() {
+//     chatBox.classList.toggle('active');
+// }
+// setTimeout(() => {
+//     showChatBox();
+//     changeColorIcon();
+// }, 10500);
